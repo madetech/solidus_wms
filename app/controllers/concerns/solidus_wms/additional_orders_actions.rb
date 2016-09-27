8bee2ed5486412f58fec83e7181a4c7edf799a87
Emile <emile.swarts123@gmail.com>
2016-09-27 14:07:31 +0100

    Remove project specific implementation

--- app/controllers/concerns/solidus_wms/additional_orders_actions.rb
+++ app/controllers/concerns/solidus_wms/additional_orders_actions.rb
@@ -3,19 +3,29 @@ module SolidusWms
     extend ActiveSupport::Concern
 
     def export_xlsx
-      tester_order_headers = ['Document Date', 'Sales Document Type', 'Purchase order number no', 'Sales Document', 'APC Local Depot No.', 'Delivery DLX No']
+      exporter = Rails.application.config.order_xls_export_class.new
 
       Mime::Type.register 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', :xlsx
 
-      xlsx_file_contents = Axlsx::Package.new do |p|
-        p.workbook.add_worksheet(name: 'Tester orders') do |sheet|
-          sheet.add_row tester_order_headers
-          Spree::Order.all.each { |order| sheet.add_row [order.created_at, 'OR', '', '', '', ''] }
-        end
-      end.to_stream.string
-      send_data(xlsx_file_contents,
-                type:        Mime::XLSX,
+      send_data(xlsx_file_contents(exporter).to_stream.string,
+                type: Mime::XLSX,
                 disposition: "attachment; filename=orders.xlsx")
+    end
+
+    private
+
+    def xlsx_file_contents(exporter)
+      Axlsx::Package.new do |p|
+        exporter.worksheets.each do |order_group|
+          p.workbook.add_worksheet(name: order_group.fetch('name')) do |sheet|
+            sheet.add_row order_group.fetch('headers')
+
+            order_group.fetch('orders').each do |order|
+              sheet.add_row order
+            end
+          end
+        end
+      end
     end
   end
 end
